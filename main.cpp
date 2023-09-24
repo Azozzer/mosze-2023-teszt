1e48056d946ef9ce4fa6462ef3cbcc2b3fc6b080
Azozzer <145788466+Azozzer@users.noreply.github.com>
2023-09-24 22:33:27 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // N_ELEMENTS-nek kéne lennie
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS]; // NELEMENTS helyett N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;
+        b[i] = i*2;
     }
-    for (int i = 0; i; i++) // i értéke nagyobb vagy egyenlő, mint N_ELEMENTS
+    std::cout << "Ertek:";
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << " " << b[i];
+    }
+    std::cout << std::endl;
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // atlag = 0
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] // ; hiánya
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
