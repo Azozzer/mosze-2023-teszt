62c72f73f2bf45f72e40dee9308ae9b5c8c7cc31
Azozzer <145788466+Azozzer@users.noreply.github.com>
2023-09-24 22:04:43 +0200

    hiba_main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    int *b = new int[NELEMENTS]; // N_ELEMENTS-nek kéne lennie
     std::cout << '1-100 ertekek duplazasa'
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // i értéke nagyobb vagy egyenlő, mint N_ELEMENTS
     {
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; // atlag = 0
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] // ; hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
